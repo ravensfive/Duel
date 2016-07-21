b0e545c07e804c02ff1e98fc64c0006644fd679a
Steven Gooday <StevenGooday@stevens-macbook-pro.local>
2016-07-21 10:12:22 +0100

    v0.002

---
+++ Duel/BrickBreaker.swift
@@ -0,0 +1,13 @@
+//
+//  BrickBreaker.swift
+//  Duel
+//
+//  Created by Steven Gooday on 21/07/2016.
+//  Copyright © 2016 DT5Ravens. All rights reserved.
+//
+
+import SpriteKit
+
+class BrickBreaker: SKScene {
+
+}
---
+++ Duel/MainMenu.swift
@@ -0,0 +1,17 @@
+//
+//  MainMenu.swift
+//  Duel
+//
+//  Created by Steven Gooday on 21/07/2016.
+//  Copyright © 2016 DT5Ravens. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenu: SKScene {
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let game:BrickBreaker = BrickBreaker(fileNamed: "BrickBreaker")!
+        self.view?.presentScene(game)
+    }
+
+}
