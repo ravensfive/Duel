9e888d93469cdd0842d10bf0001b6325bd58b2d4
Steven Gooday <StevenGooday@stevens-macbook-pro.local>
2016-08-02 10:37:40 +0100

    Brick Contact

--- Duel/Bricks.swift
+++ Duel/Bricks.swift
@@ -8,21 +8,34 @@
 import SpriteKit
 import UIKit
 
-class Bricks: SKScene {
+let BallCategory:UInt32 = 0x1 << 0 // 1
+let BrickCategory:UInt32 = 0x1 << 1 // 2
+
+class Bricks: SKScene, SKPhysicsContactDelegate {
 
     var Ball:SKSpriteNode!
     var Paddle:SKSpriteNode!
+    var Brick:SKSpriteNode!
     var TouchLocation:CGPoint = CGPointZero
     
     override func didMoveToView(view: SKView) {
         Ball = self.childNodeWithName("Ball") as! SKSpriteNode
         Paddle = self.childNodeWithName("Paddle") as! SKSpriteNode
+        Brick = self.childNodeWithName("Brick_1") as! SKSpriteNode
         
+        self.physicsWorld.contactDelegate = self
+        
+        Ball.physicsBody?.applyImpulse(CGVector(dx: 100, dy: 250))
+        
+        Ball.physicsBody!.categoryBitMask = BallCategory
+        Brick.physicsBody!.categoryBitMask = BrickCategory
+        
+        Ball.physicsBody!.contactTestBitMask = BrickCategory
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         TouchLocation = touches.first!.locationInNode(self)
-        //Ball.physicsBody?.applyImpulse(CGVector(dx: 100, dy: 2500))
+        
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -32,7 +45,18 @@ class Bricks: SKScene {
     
     override func update(currentTime: CFTimeInterval) {
         Paddle.position.x = TouchLocation.x
-        // golf
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        
+    
+        if contact.bodyA.categoryBitMask == BrickCategory {
+                contact.bodyA.node!.removeFromParent()
+        }
+    
+        print(contact.bodyA.categoryBitMask)
+        print(contact.bodyB.categoryBitMask)
+        
     }
     
 }
