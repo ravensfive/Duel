38b65bd7850e667be0c4e915f3263b91bb265405
Steven Gooday <StevenGooday@stevens-macbook-pro.local>
2016-08-02 15:52:00 +0100

    Two Bricks

--- Duel/Bricks.swift
+++ Duel/Bricks.swift
@@ -15,20 +15,27 @@ class Bricks: SKScene, SKPhysicsContactDelegate {
 
     var Ball:SKSpriteNode!
     var Paddle:SKSpriteNode!
-    var Brick:SKSpriteNode!
+    var Brick_1:SKSpriteNode!
+    var Brick_2:SKSpriteNode!
     var TouchLocation:CGPoint = CGPointZero
     
     override func didMoveToView(view: SKView) {
         Ball = self.childNodeWithName("Ball") as! SKSpriteNode
         Paddle = self.childNodeWithName("Paddle") as! SKSpriteNode
-        Brick = self.childNodeWithName("Brick_1") as! SKSpriteNode
+        Brick_1 = self.childNodeWithName("Brick_1") as! SKSpriteNode
+        Brick_2 = self.childNodeWithName("Brick_2") as! SKSpriteNode
         
         self.physicsWorld.contactDelegate = self
         
         Ball.physicsBody?.applyImpulse(CGVector(dx: 100, dy: 250))
+        Ball.physicsBody?.friction = 0
+        Ball.physicsBody?.restitution = 1
+        Ball.physicsBody?.linearDamping = 0
+        Ball.physicsBody?.angularDamping = 0
         
         Ball.physicsBody!.categoryBitMask = BallCategory
-        Brick.physicsBody!.categoryBitMask = BrickCategory
+        Brick_1.physicsBody!.categoryBitMask = BrickCategory
+        Brick_2.physicsBody!.categoryBitMask = BrickCategory
         
         Ball.physicsBody!.contactTestBitMask = BrickCategory
     }
@@ -48,7 +55,6 @@ class Bricks: SKScene, SKPhysicsContactDelegate {
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
-        
     
         if contact.bodyA.categoryBitMask == BrickCategory {
                 contact.bodyA.node!.removeFromParent()
