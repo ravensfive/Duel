d5cf6583c528d2e8bbf43e76f9fd76f15496287d
Steven Gooday <StevenGooday@stevens-macbook-pro.local>
2016-08-12 08:27:02 +0100

    GamesStates Folder

---
+++ Duel/WaitingForTap.swift
@@ -0,0 +1,14 @@
+//
+//  WaitingForTap.swift
+//  Duel
+//
+//  Created by Steven Gooday on 12/08/2016.
+//  Copyright © 2016 DT5Ravens. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class WaitingForTap: GKState {
+
+}
