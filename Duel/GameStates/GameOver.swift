66f4285778f25c28bd27e34d231a30523a9f75fd
Steven Gooday <StevenGooday@stevens-macbook-pro.local>
2016-08-12 08:22:04 +0100

    Testing adding Gamestates

---
+++ Duel/GameStates/GameOver.swift
@@ -0,0 +1,14 @@
+//
+//  GameOver.swift
+//  Duel
+//
+//  Created by Steven Gooday on 12/08/2016.
+//  Copyright © 2016 DT5Ravens. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class GameOver: GKState {
+
+}
